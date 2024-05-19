de510c95e71bdbecffb761f8b183f03b9f692c0c
Julia Ilasova <1julka1il@gmail.com>
2024-05-19 16:30:45 +0200

    placeholder image events

---
+++ api/Events/PlantEvents/Client/ClientWantsPlaceholderUrl.cs
@@ -0,0 +1,26 @@
+using api.Events.PlantEvents.Server;
+using api.Extensions;
+using Core.Options;
+using Core.Services.External.BlobStorage;
+using Fleck;
+using lib;
+using Microsoft.Extensions.Options;
+using Shared.Models;
+
+namespace api.Events.PlantEvents.Client;
+
+public class ClientWantsPlaceholderUrlDto : BaseDtoWithJwt;
+
+public class ClientWantsPlaceholderUrl(IOptions<AzureBlobStorageOptions> blobOptions, IBlobStorageService blobStorageService) : BaseEventHandler<ClientWantsPlaceholderUrlDto>
+{
+    public override Task Handle(ClientWantsPlaceholderUrlDto dto, IWebSocketConnection socket)
+    {
+        var placeholderUrl = blobOptions.Value.DefaultPlantImageUrl;
+        var sasUri = blobStorageService.GenerateSasUri(placeholderUrl);
+        socket.SendDto(new ServerSendsPlaceholderUrl
+        {
+            PlaceholderUrl = sasUri
+        });
+        return Task.CompletedTask;
+    }
+}
