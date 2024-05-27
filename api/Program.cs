253bb4ac24a427e08d8de7c483ffe6e4495a2668
Julia Ilasova <1julka1il@gmail.com>
2024-05-27 10:33:21 +0200

    comment out mqtt subscription

--- api/Program.cs
+++ api/Program.cs
@@ -151,9 +151,11 @@ public static class Startup
             };
         });
         
+        /*
         // Connect and subscribe to MQTT
         var mqttSubscriberService = app.Services.GetRequiredService<MqttSubscriberService>();
         _ = mqttSubscriberService.SubscribeAsync();
+        */
 
         return app;
     }
