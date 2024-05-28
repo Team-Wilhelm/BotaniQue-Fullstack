a559127e0fc013485fef4139df38a91286382e62
Julia Ilasova <1julka1il@gmail.com>
2024-05-28 13:59:24 +0200

    ripped off websockettestclient to see if the timing is the problem

---
+++ Tests/WebSocketTestClient.cs
@@ -0,0 +1,56 @@
+using System.Text.Json;
+using lib;
+using Websocket.Client;
+
+namespace Tests
+{
+    public class WebSocketTestClient
+    {
+        public readonly WebsocketClient Client;
+        public readonly List<BaseDto> ReceivedMessages = [];
+
+        public WebSocketTestClient(string? url = null)
+        {
+            Client = url == null ? new WebsocketClient(new Uri("ws://localhost:" + (Environment.GetEnvironmentVariable("FULLSTACK_API_PORT") ?? "8181"))) : new WebsocketClient(new Uri(url));
+            Client.MessageReceived.Subscribe((Action<ResponseMessage>) (msg =>
+            {
+                BaseDto baseDto = JsonSerializer.Deserialize<BaseDto>(msg.Text);
+                lock (ReceivedMessages)
+                    ReceivedMessages.Add(baseDto);
+            }));
+        }
+
+        public async Task<WebSocketTestClient> ConnectAsync()
+        {
+            await Client.Start();
+            if (!Client.IsRunning)
+                throw new Exception("Could not start client!");
+            return this;
+        }
+
+        public void Send<T>(T dto) where T : BaseDto
+        {
+            Client.Send(JsonSerializer.Serialize(dto));
+        }
+
+        public async Task DoAndAssert<T>(T? action = null, Func<List<BaseDto>, bool>? condition = null) where T : BaseDto
+        {
+            if ((object) (T) action != null)
+                Send(action);
+            if (condition != null)
+            {
+                DateTime startTime = DateTime.UtcNow;
+                while (DateTime.UtcNow - startTime < TimeSpan.FromSeconds(10.0))
+                {
+                    lock (ReceivedMessages)
+                    {
+                        if (condition(ReceivedMessages))
+                            return;
+                    }
+                    await Task.Delay(100);
+                }
+                throw new TimeoutException("Condition not met: ");
+            }
+        }
+    }
+}
